929fb04ed6a889ec400a57a4c9b519f27eb799d5
MotivatedUser <carbfixer@gmail.com>
2023-01-28 13:08:14 -0800

    Workshop 3

--- models/user.js
+++ models/user.js
@@ -19,4 +19,7 @@ const userSchema = new Schema({
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
